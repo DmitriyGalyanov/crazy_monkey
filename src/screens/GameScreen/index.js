f08f5badf8e4a76fde2d483f63fd623ff0501a48
DmitriyGalyanov <dimagalyanov@gmail.com>
2020-11-26 13:14:47 +0300

    Engine registers touches. init, class

--- src/screens/GameScreen/index.js
+++ src/screens/GameScreen/index.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { Component, PureComponent } from "react";
+import { AppRegistry, StyleSheet, StatusBar, SafeAreaView, View, Alert, Button, TouchableOpacity } from "react-native";
+import { GameEngine, dispatch } from "react-native-game-engine";
 
-import {
-	StyleSheet,
-	View,
-} from 'react-native';
+import { Finger } from './renderers';
+import GameLoop from './systems';
 
-import Test from 'components/Test';
+export default class GameScreen extends Component {
+		constructor(props) {
+				super(props);
+				// this.engine = null;
+				this.state = {
+						running: true
+				}
+		}
 
+		render() {
+				return (
+						<View style={styles.container}>
+								<GameEngine
+										// ref={(ref) => { this.engine = ref; }}
+										style={styles.container}
+										systems={[ GameLoop ]}
+										entities={{
+											1: { position: [40,  200], renderer: <Finger />}, //-- Notice that each entity has a unique id (required)
+											2: { position: [100, 200], renderer: <Finger />}, //-- and a renderer property (optional). If no renderer
+											3: { position: [160, 200], renderer: <Finger />}, //-- is supplied with the entity - it won't get displayed.
+											4: { position: [220, 200], renderer: <Finger />},
+											5: { position: [280, 200], renderer: <Finger />}
+										}}
+										running={this.state.running}
+										onEvent={this.onEvent}>
 
-export default function GameScreen() {
+										<StatusBar hidden={true} />
 
-	return (
-		<View style={styles.wrap}>
-			<Test />
-		</View>
-	)
+								</GameEngine>
+						</View>
+				);
+		}
 }
 
 const styles = StyleSheet.create({
-	wrap: {
-
-	},
+		container: {
+				flex: 1,
+				backgroundColor: '#000000',
+		},
 });
